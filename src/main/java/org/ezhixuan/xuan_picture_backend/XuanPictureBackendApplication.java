b78bec6b349bc5c32394e4df6126910cd0fd98f0
大魔王某某轩🤩 <ezhixuan@damowangmoumouxuandeMacBook-Pro.local>
2025-03-02 17:38:21 +0800

    Initial commit

---
+++ src/main/java/org/ezhixuan/xuan_picture_backend/XuanPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ezhixuan.xuan_picture_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuanPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuanPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ezhixuan/xuan_picture_backend/XuanPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ezhixuan.xuan_picture_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuanPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
